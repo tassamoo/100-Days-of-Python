96281413ffd1735f3c31a98aa01a7ee75260c6c5
tassamoo <tassamu@outlook.com>
2021-03-06 21:34:37 +0700

    Add code on Day 10

---
+++ Day 10 - Function with outputs.py
@@ -0,0 +1,15 @@
+#Function with Outputs
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+
+    return f"{formatted_f_name} {formatted_l_name}"
+
+formatted_string = format_name("tassamu", "AKHSAN")
+
+print(formatted_string)
+
+output = len("Tassamu")
+
+print(output)
